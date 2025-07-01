649f2b1a01ec112a83f7426a702ca1d7c19320b8
mohitmishra12 <mohitmishra747041@gmail.com>
2025-07-01 18:51:17 +0530

    Test with mockito

--- src/test/java/org/example/Servicess/CalculaterServicessTest.java
+++ src/test/java/org/example/Servicess/CalculaterServicessTest.java
@@ -1,20 +1,36 @@
 package org.example.Servicess;
 
 import junit.framework.Assert;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 public class CalculaterServicessTest {
 
+    @BeforeAll
+    public static void init(){
+        System.out.println("welcome to Unit Testing Screen");
+    }
+//    @AfterEach
+//    public void Afterachh(){
+//        System.out.println("After test case");
+//    }
+//
+//    @BeforeEach
+//    public void BeforeEcahh(){
+//        System.out.println("Before test case");
+//    }
+
     @Test
+    @DisplayName("this is our add method")
     public void addTwoNumberTest(){
+        System.out.println("this is my add two number testing");
         int result = CalculaterServicess.addTwoNumber(12,34);
         int expected = 46;
-        Assertions.assertEquals(expected,result);
+        Assertions.assertEquals(expected,result,"test case fail");
     }
 
     @Test
     public void productTwoNumberTest(){
+        System.out.println("this is my multi two number testing");
         int result = CalculaterServicess.productTwoNumber(12,12);
         int expected = 144;
         Assertions.assertEquals(expected,result);
@@ -22,15 +38,25 @@ public class CalculaterServicessTest {
 
     @Test
     public void sumAnyNumberTest(){
+        System.out.println("this is my Sum any number testing");
         int result = CalculaterServicess.sumAnyNumber(1,2,3,5,6);
         int expected = 17;
         Assertions.assertEquals(expected,result);
     }
 
     public void AddFlotValueTest(){
+        System.out.println("this is my Float value test two number testing");
         float result = CalculaterServicess.AddFlotValue(12.50F, 12.50F);
         int expected = 50;
         Assertions.assertEquals(expected,result);
     }
+
+
+    @AfterAll
+    public static void Cleanup(){
+        System.out.println("Our all test case are logic");
+    }
+
+
 
 }
